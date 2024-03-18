c9252bb6e98642783c2fafe108977ee510d71e7a
Minh <minhvuongakira@gmail.com>
2024-03-18 21:03:38 +0700

    Load ds luận văn các thầy cô khác

--- Quan_Li_Luan_Van/FDanhSachLuanVan.cs
+++ Quan_Li_Luan_Van/FDanhSachLuanVan.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace Quan_Li_Luan_Van
 {
     public partial class FDanhSachLuanVan : Form
     {
+        SqlConnection conn = new SqlConnection(Properties.Settings.Default.cnnStr);
+        string query = "SELECT LuanVan.TenLV, LuanVan.ChuyenNganh, GiangVien.TenGV, LuanVan.SoLuongConLai " +
+                                   "FROM LuanVan " +
+                                   "JOIN GiangVien ON LuanVan.MaGV = GiangVien.MaGV ";
         public FDanhSachLuanVan()
         {
             InitializeComponent();
@@ -19,8 +24,33 @@ namespace Quan_Li_Luan_Van
 
         private void TraCuu_Load(object sender, EventArgs e)
         {
-            UCLV uCLV = new UCLV();
-            flowPanelLuanVan.Controls.Add(uCLV);
+            try
+            {
+                conn.Open();
+                flowPanelLuanVan.Controls.Clear();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader dataReader = cmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    UCLV uclv = new UCLV();
+
+                    uclv.LbTenLV.Text = dataReader["TenLV"].ToString();
+                    uclv.LbChuyenNganh.Text = dataReader["ChuyenNganh"].ToString();
+                    uclv.LblTenGV.Text = dataReader["TenGV"].ToString();
+                    uclv.LbSoLuong.Text = dataReader["SoLuongConLai"].ToString();
+
+                    flowPanelLuanVan.Controls.Add(uclv);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
